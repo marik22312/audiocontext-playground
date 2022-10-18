65fc17923be3db6ed15478a48a222c037273d9d4
Marik Shnitman <mark22312@gmail.com>
2022-10-18 13:30:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-export default {
-	base: '/audiocontext-playground/'
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/audiocontext-playground/'
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        drumkit: resolve(__dirname, 'drumkit/index.html'),
+	['microphone-visualiser']: resolve(__dirname, 'microphone-visualiser/index.html'),
+	note: resolve(__dirname, 'note/index.html'),
+      }
+    }
+  }
+})
