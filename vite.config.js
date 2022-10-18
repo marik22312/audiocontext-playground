ecfd1aca73bcafbb9579ad9d16e257108e2b6618
Marik Shnitman <mark22312@gmail.com>
2022-10-18 13:31:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/audiocontext-playground/'
+  base: '/audiocontext-playground/',
   build: {
     rollupOptions: {
       input: {
